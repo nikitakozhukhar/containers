95e66a5de7ef342243e113f78bdab21bc4a57aba
NikitaKx9 <nikitakx9@gmail.com>
2023-06-26 10:46:56 +0300

    errRepo.js changed

--- src/js/__tests__/errorrepository.test.js
+++ src/js/__tests__/errorrepository.test.js
@@ -2,14 +2,21 @@ import ErrorRepository from "../class/ErrorRepository";
 
 let er = new ErrorRepository();
 
-test('Выбрасывает ошибку из коллекции Map', () => {
+test('Читает и выдает ошибку по входящему коду из коллекции Map', () => {
 	const error = er.translate('1');
 
-	expect('Данный персонаж уже добавлен в команду').toThrow(error)
+	const check = 'Данный персонаж уже добавлен в команду'
+
+	expect(check).toThrow(error) // 1й вариант проверки
+
+	expect('Данный персонаж уже добавлен в команду').toThrow(error) //2й варинт проверки
 })
 
-test('Выбрасывает ошибку из коллекции Map', () => {
+test('Выбрасывает ошибку из-за отсутствия кода в коллекции Map', () => {
 	const error = er.translate('3');
 
-	expect('Неизвестная ошибка').toThrow(error)
+	expect(() => {
+		return 'Неизвестная ошибка'
+	}).toThrow(error)  // 3й вариант проверки
+	// expect('Неизвестная ошибка').toThrow(error)
 })
